be3e10a69eb0f9a859c5f8af2794dd57c1137634
TonyShanc <shanah01@foxmail.com>
2021-12-30 14:34:18 +0800

    perf: main.go

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/TonyShanc/fund-strategy/model"
+)
+
 func main() {
-	
+	stras := model.LoadStrategy()
+	for _, substra := range stras.Strategies {
+		for _, ana := range substra.GenAnalysis() {
+			if ifNotice := ana.Analyze(); ifNotice {
+				fmt.Println(ana.Msg)
+			}
+		}
+	}
 }
